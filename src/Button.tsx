d98d90e234cd4376d57425d7b3cae5181bfdad4a
Hallef Santos <hallefsantos@gmail.com>
2025-10-15 11:29:49 -0300

    Update Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,5 @@
+import './Button.css';
+
 export default function Button() {
   return <div style={{ display: 'inline-block', backgroundColor: 'green', color: 'white', padding: 4, borderRadius: 6 }}>Provider button NIK</div>;
 }

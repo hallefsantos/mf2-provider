cc4270197a5da2ee2b7abe44677abd2dd6b94a67
Hallef Santos <hallefsantos@gmail.com>
2025-10-14 23:51:31 -0300

    genesis

---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'federation_provider',
+      exposes: {
+        './button': './src/Button.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  output: {
+    assetPrefix: 'auto',
+  },
+});

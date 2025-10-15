7346ba0b3891e6b72b9b376da941070ca6362e89
Hallef Santos <hallefsantos@gmail.com>
2025-10-15 11:23:47 -0300

    Update Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,3 @@
 export default function Button() {
-  return <div style={{ display: 'inline-block', backgroundColor: 'blue', color: 'white', padding: 4, borderRadius: 6 }}>Provider button AMAZING</div>;
+  return <div style={{ display: 'inline-block', backgroundColor: 'green', color: 'white', padding: 4, borderRadius: 6 }}>Provider button NIK</div>;
 }
